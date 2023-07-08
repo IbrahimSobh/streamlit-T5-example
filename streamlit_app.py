eba764442d05e509662c185e7ddeb6d3e2eebc27
Ibrahim Sobh <im_sobh@hotmail.com>
2023-07-08 07:14:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ Egypt has thousands of years of recorded history. Ancient Egypt was among the ea
 was the site of one of the world’s earliest urban and literate societies.\
 Ancient Egypt was the preeminent civilization in the Mediterranean world for almost 30 centuries. \
 Egypt has a rich history & culture that dates back thousands of years ago starting with the Pharaohnic culture. \
-Egypt is a popular tourist destination with many attractions. Some of the popular tourist destinations are Cairo, Sharm El Sheikh, Hurghada, Luxor, Aswan"  
+Egypt is a popular tourist destination with many attractions. Some of the popular tourist destinations are Cairo, Sharm El Sheikh, Hurghada, Luxor and Aswan."  
 
 result = []
 with st.form('summarize_form', clear_on_submit=True):
@@ -28,7 +28,7 @@ with st.form('summarize_form', clear_on_submit=True):
             summary_txt = summarizer(
                 txt_input,
                 min_length=8,
-                max_length=48,
+                max_length=64,
                 no_repeat_ngram_size=3,
                 encoder_no_repeat_ngram_size=3,
                 repetition_penalty=3.5,
