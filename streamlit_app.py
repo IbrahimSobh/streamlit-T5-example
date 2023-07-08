3408bdacb28e7036a674cc4b0cd7d43d3691f244
Ibrahim Sobh <im_sobh@hotmail.com>
2023-07-08 04:42:27 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,8 @@ st.title('🎈 App Name')
 
 st.write('Hello world!')
 
+st.echo(code_location='below')
+
 # with st.echo(code_location='below'):
 #     total_points = st.slider("Sobh: Number of points in spiral", 1, 5000, 500)
 #     num_turns = st.slider("Sobh: Number of turns in spiral", 1, 100, 9)
