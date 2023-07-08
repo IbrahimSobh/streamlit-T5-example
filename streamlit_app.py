9d4ab3a36f57f25600d9b74c19e343945684b7fb
Ibrahim Sobh <im_sobh@hotmail.com>
2023-07-08 10:06:20 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ For more, check out my LinkedIn [Ibrahim Sobh](https://www.linkedin.com/in/ibrah
 """
 
 st.set_page_config(page_title='Faln-T5 model 🤗 🚀')
-
+st.title('Faln-T5 model 🤗 🚀')
 
 
 tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-small")
